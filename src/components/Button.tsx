fd2f00ac1cb27695669ccca0d9072d19fc85a250
Ashis <amaity@digisprint.com>
2023-10-19 22:36:04 +0530

    feat(form handling): implemented onsubmit using form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@
 // How we are handling props in TS.
 // How to use useState hooks?
 
-import { useState } from "react";
+import React, { useState } from "react";
 
 interface MyButtonProps {
   //    text: string;   // Here text is required.
@@ -44,23 +44,55 @@ interface MyButtonProps {
 // explicitly generic as a custom object and using interface we can manage our state.
 // explicit always a good ideas.
 
-interface Book {
-  name: string;
-  price: number;
-}
+// interface Book {
+//   name: string;
+//   price: number;
+// }
 
+// const MyButton: React.FC<MyButtonProps> = (props) => {
+//   const { onClick, text } = props;
+
+//   const [value, setValue] = useState<Book>({
+//     name: 'Mahalaya',
+//     price: 1000,
+//   });
+
+//   return (
+//     <div>
+//       <h3>Name: {value.name} (Rs. {value.price} )</h3>
+//       <button onClick={() => setValue({name: "DurgaPuja", price: 400})}>{text}</button>
+//     </div>
+//   );
+// };
+
+// form handling
 const MyButton: React.FC<MyButtonProps> = (props) => {
   const { onClick, text } = props;
 
-  const [value, setValue] = useState<Book>({
-    name: 'Mahalaya',
-    price: 1000,
-  });
+  const [value, setValue] = useState<string | undefined>(); // my state can be either string or undifined
+
+  // For onChange we have ChangeEvent || for onclick we have ClickEvent
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  // If you hover onSubmit we can get to know what even it was..
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
-      <h3>Name: {value.name} (Rs. {value.price} )</h3>
-      <button onClick={() => setValue({name: "DurgaPuja", price: 400})}>{text}</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onChange={handleNameChange}
+          value={value}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <h3>{value}</h3>
     </div>
   );
 };
